28ba8fe9282480b8a2b73cd77ac000e787d4834d
Szymon <sszulinski@gmail.com>
2021-06-16 05:34:19 +0200

    Move datebase and add testimonial routes

--- server.js
+++ server.js
@@ -1,64 +1,98 @@
 const express = require('express');
 const path = require('path');
-import { v4 as uuidv4 } from 'uuid';
-
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
-
+const { v4: uuidv4 } = require('uuid');
+const db = require('./db');
 const app = express();
+const testimonialsRoutes = require('./routes/testimonials.routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/api', testimonialsRoutes);
 
-app.get('/testimonials/', (req,res) => {
-  res.json(db);
+app.get('/concerts', (req,res) => {
+    res.json(db.concerts);
+  });
+
+  app.get('/concerts/:id', (req,res) => {
+    res.json(db.concerts.find(item => item.id == req.params.id));
+  });
+
+  app.post('/concerts/', (req,res) => {
+    const concert = {
+      id: uuidv4(),
+      performer: req.body.performer,
+      genre: req.body.genre,
+      price: req.body.price,
+      day: req.body.day,
+      image: req.body.image
+    };
+    db.concerts.push(concert);
+    return res.json(concert);
+  });
+
+  app.delete('/concerts/:id', (req,res) => {
+    db.concerts.forEach(concert => {
+      if(concert.id == req.params.id) {
+        const index = db.concerts.indexOf(concert);
+        db.concerts.splice(index,1);
+      }
+    });
+    return res.json(db.concerts);
 });
 
-app.get('/testimonials/random/', (req,res) => {
-  let item = db[Math.floor(Math.random() * db.length)];
-  res.json(item);
+app.put('/concerts/:id', (req,res) => {
+    db.concerts.forEach(concert => {
+      if(concert.id == req.params.id) {
+        concert.performer = req.body.performer,
+        concert.genre = req.body.genre,
+        concert.price = req.body.price,
+        concert.day = req.body.day,
+        concert.image = req.body.image
+      }
+    });
+    return res.json(db.concerts);
 });
 
-app.get('/testimonials/:id', (req,res) => {
-  res.json(db.find(item => item.id == req.params.id));
+app.get('/seats', (req,res) => {
+  res.json(db.seats);
 });
 
-app.post('/testimonials/', (req,res) => {
-  const obj = {
+app.get('/seats/:id', (req,res) => {
+  res.json(db.concerts.find(seat => seat.id == req.params.id));
+});
+
+app.post('/seats', (req,res) => {
+  const seat = {
     id: uuidv4(),
-    author: req.body.author,
-    text: req.body.text
-  }
-  db.push(obj);
-  return res.json({
-    message: 'ok'
-  });
+    day: req.body.day,
+    seat: req.body.seat,
+    client: req.body.client,
+    email: req.body.email
+  };
+  db.seats.push(seat);
+  return res.json(seat);
 });
 
-app.put('/testimonials/:id', (req,res) => {
-  db.forEach(element => {
-    if(element.id == req.params.id) {
-      element.author = req.body.author;
-      element.text = req.body.text;
+app.delete('/seats/:id', (req,res) => {
+  db.seats.forEach(seat => {
+    if(seat.id == req.params.id) {
+      const index = db.seats.indexOf(seat);
+      db.seats.splice(index,1);
     }
-  });
-  return res.json({
-    message: 'ok'
-  });
+  })
+  return res.json(db.seats);
 });
 
-app.delete('/testimonials/:id', (req,res) => {
-  db.forEach(element => {
-    if(element.id == req.params.id) {
-      const index = db.indexOf(element);
-      db.splice(index,1);
+app.put('/seats/:id', (req,res) => {
+  db.seats.forEach(seat => {
+    if(seat.id == req.params.id){
+      seat.day = req.body.day,
+      seat.seat = req.body.seat,
+      seat.client = req.body.client,
+      seat.email = req.body.email
     }
   });
-  return res.json({
-    message: 'ok'
-  });
+  return res.json(db.seats);
 });
 
 app.use((req,res) => {
@@ -67,7 +101,6 @@ app.use((req,res) => {
   });
 });
 
-
 app.listen('8000',() => {
   console.log(path.join(__dirname, 'Server is running on port 8000'));
-}); 
+});
